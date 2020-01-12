4de97f8bcfd562604093741725947c4ef0b19cf6
Glitch (sour-sandal) <none>
2020-01-12 09:44:46 +0000

    🐜💳 Checkpoint ./commands/play.js:4408904/2

--- commands/play.js
+++ commands/play.js
@@ -11,7 +11,7 @@ module.exports = {
   async execute(message, args) {
     const { channel } = message.member.voice;
 
-    if (!args.length) return message.reply("Usage: /play <YouTube URL | Video Name>").catch(console.error);
+    if (!args.length) return message.reply("Usage: !play <YouTube URL | Video Name>").catch(console.error);
     if (!channel) return message.reply("You need to join a voice channel first!").catch(console.error);
 
     const permissions = channel.permissionsFor(message.client.user);
